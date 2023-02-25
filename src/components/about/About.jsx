47f715cb2264818574a61a48c192c3030173732c
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-02-25 18:38:52 +0500

    Add About section
    In this commit
    - Add about-section for the portfolio app.
    - Add styles to the about section.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/me-about.jpg';
+import {FaAward} from'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
     <section id='about'>
-      
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Collaboration Partners</h5>
+              <small>10+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>20+ Completed</small>
+            </article>
+          </div>
+
+          <p>I can help you build a product, feature or website. Look through some of my work and experience!. If you like whay you need coded, don't hesitate to cintact me.</p>
+          <a href="contact" className='btn btn-primary'>Let's talk</a>
+        </div>
+      </div>
     </section>
   )
 }
