8c5483aa0b90270077e9056e430106ec0eb28ada
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-02-25 17:05:17 +0500

    Add header component
    In this commit
    - Add a Header component for the app.
    - Add styles for the Header component.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import ME from'../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container"><h5>Hello I'm</h5>
+        <h1>Wahaj Ali</h1>
+        <h5 className='text-light'>Fullstack Developr</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+        </div>
+    </header>
   )
 }
 
